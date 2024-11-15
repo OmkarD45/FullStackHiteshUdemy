e362feebfb3aad0404427ab47cd6bb0f2c94fb9e
OmkarD45 <omkardeshmukh162001@gmail.com>
2024-11-15 13:02:58 +0530

    15112024 Arrow functions

--- JavascriptFoundation/hello.js
+++ JavascriptFoundation/hello.js
@@ -19,3 +19,11 @@ function orderTea(type){
     return confirmOrder();
 }
 console.log(orderTea("Lemon Tea"));
+
+// ___________________________________________________________
+// Arrow Functions
+const calculateTotal=(price,qty)=>{
+    return price*qty;
+}
+
+console.log(`Total Price ${calculateTotal(200,5)}`);
