56083b263bef2460089cdc52e1d65322b0e6a240
OmkarD45 <omkardeshmukh162001@gmail.com>
2024-11-15 12:41:55 +0530

    15112024 functions

--- JavascriptFoundation/hello.js
+++ JavascriptFoundation/hello.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+// console.log("Hello");
+
+function greet(name){
+    console.log(`Hello ${name}`);
+}
+
+greet("Omkar");
+
+function makeTea(type){
+    return `Making ${type}`;
+}
+console.log(makeTea("Lemon Tea"));
+
+
+function orderTea(type){
+    function confirmOrder(){
+        return `Order confirmed`;
+    }
+    return confirmOrder();
+}
+console.log(orderTea("Lemon Tea"));
